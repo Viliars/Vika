32d3fc896a8b9ab42be66e5c1d2e0890d6c0c38d
Viliars <redkillerpw@mail.ru>
2020-02-13 12:14:45 +0300

    frist message from server

--- app/routes.py
+++ app/routes.py
@@ -14,12 +14,12 @@ vk = vk_session.get_api()
 def root():
     f = request.get_json()
     if f["object"]["message"]["text"] == "/sayhello":
-        from_id = f["object"]["message"]["from_id"]
+        peer_id = f["object"]["message"]["peer_id"]
         # отправляем сообщение
         vk.messages.send(
             message=u'Привет, Я Вика - новый бот, названный в честь виртуального помощника из романа Сергея Лукьяненко "Лабиринт отражений"',
             random_id=get_random_id(),
-            peer_id=from_id
+            peer_id=peer_id
         )
 
     return "ok", 200
