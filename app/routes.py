06cb3b4165e5ae1a121378f2d81afd3b1afec8d0
Viliars <redkillerpw@mail.ru>
2020-02-13 11:58:53 +0300

    test get info from post request

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
-
+from flask import request
 
 @app.route('/', methods=['POST'])
 def root():
+    f = request.json()
+    with open("test.txt", "w") as fout:
+        print(f, file=fout)
     return "ok", 200
