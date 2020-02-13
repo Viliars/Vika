3430cd9a34716907533d2daa39620481635cd85d
Viliars <redkillerpw@mail.ru>
2020-02-13 12:13:56 +0300

    fix error

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,7 @@ vk = vk_session.get_api()
 def root():
     f = request.get_json()
     if f["object"]["message"]["text"] == "/sayhello":
-        from_id = f['object']['from_id']
+        from_id = f["object"]["message"]["from_id"]
         # отправляем сообщение
         vk.messages.send(
             message=u'Привет, Я Вика - новый бот, названный в честь виртуального помощника из романа Сергея Лукьяненко "Лабиринт отражений"',
