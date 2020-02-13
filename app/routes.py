bd3e39c8727326ca2b2d34c3a587d36d609d7e08
Viliars <redkillerpw@mail.ru>
2020-02-13 12:12:53 +0300

    fix error

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,25 @@
+# -*- coding: utf-8 -*-
 from app import app
 from flask import request
+import vk_api
+from vk_api.utils import get_random_id
+
+with open("key") as key_file:
+    key = key_file.read().strip()
+
+vk_session = vk_api.VkApi(token=key)
+vk = vk_session.get_api()
 
 @app.route('/', methods=['POST'])
 def root():
     f = request.get_json()
-    #["object"]["message"]["text"]
-    with open("test.txt", "a") as fout:
-        print(f, file=fout)
+    if f["object"]["message"]["text"] == "/sayhello":
+        from_id = f['object']['from_id']
+        # отправляем сообщение
+        vk.messages.send(
+            message=u'Привет, Я Вика - новый бот, названный в честь виртуального помощника из романа Сергея Лукьяненко "Лабиринт отражений"',
+            random_id=get_random_id(),
+            peer_id=from_id
+        )
+
     return "ok", 200
