2acc95c8e527a3c2c702a7e50380b7d3b54b07b3
Viliars <redkillerpw@mail.ru>
2020-02-20 19:41:31 +0300

    New feature

--- app/routes.py
+++ app/routes.py
@@ -2,24 +2,48 @@
 from app import app
 from flask import request
 import vk_api
+from vk_api import VkUpload
 from vk_api.utils import get_random_id
+from rank_bm25 import BM25Okapi
+import numpy as np
 
 with open("key") as key_file:
     key = key_file.read().strip()
 
+corpus = [
+    "ну да ну да пошел я нахер",
+    "назад в дурку",
+    "этот маленький маневр обойдется нам в 51 год"
+]
+
+tokenized_corpus = [doc.split(" ") for doc in corpus]
+
+bm25 = BM25Okapi(tokenized_corpus)
+
 vk_session = vk_api.VkApi(token=key)
+upload = VkUpload(vk_session)
 vk = vk_session.get_api()
 
 @app.route('/', methods=['POST'])
 def root():
     f = request.get_json()
-    if f["object"]["message"]["text"] == "/sayhello":
-        peer_id = f["object"]["message"]["peer_id"]
-        # отправляем сообщение
+    text: str = f["object"]["message"]["text"]
+    peer_id = f["object"]["message"]["peer_id"]
+    if text == "/sayhello":
         vk.messages.send(
             message=u'Привет, я Вика - новый бот, названный в честь виртуального помощника из романа Сергея Лукьяненко "Лабиринт отражений"',
             random_id=get_random_id(),
             peer_id=peer_id
         )
+
+    if text.startswith('!'):
+        phrase: str = text[1:].strip().split()
+        number = np.argmax(bm25.get_scores(phrase))
+
+        photo = upload.photo_messages(photo=f"images/{number}.jpg")
+        attachment = f"photo{photo['owner_id']}_{photo['id']}"
+
+        vk.messages.send(peer_id=peer_id, attachment=attachment, random_id=get_random_id())
+
 
     return "ok", 200
